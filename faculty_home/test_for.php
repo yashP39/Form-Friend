57a52861ef0fe914dd2302896d973cc7cf937fbc
yash3915 <124658157+yashP39@users.noreply.github.com>
2023-04-01 11:46:59 +0530

    new table in db chnaged

--- faculty_home/test_for.php
+++ faculty_home/test_for.php
@@ -7,8 +7,6 @@ $json_data = file_get_contents($api_url);
 
 // Decode JSON data into PHP array
 $response_data = json_decode($json_data);
-
-print_r($response_data);
 echo nl2br("\n",false);
 
 $con=mysqli_connect("localhost","root","","404_found");
@@ -20,17 +18,11 @@ $con=mysqli_connect("localhost","root","","404_found");
     
     else
     {
-        $sql1 = "select firstname,lastname FROM student WHERE email='$response_data'";
-        $result1 = mysqli_query($con, $sql1);
 
-        $row1 = mysqli_fetch_assoc($result1);
-
-        if (mysqli_num_rows($result1) > 0) {
-            // print_r($row1['firstname']);
-
-            
+        for ($x = 1; $x <= mysqli_num_rows($response_data); $x++) {
+            $sql1 = "INSERT INTO followup_mail VALUES ('$response_data[$x]')";
+            $result1 = mysqli_query($con, $sql1);    
         }
-
 
 
 //         $sql2 = "select lastname FROM student WHERE email='$response_data[0]'";
@@ -47,24 +39,5 @@ $con=mysqli_connect("localhost","root","","404_found");
 
     }    
 
-
-// All user data exists in 'data' object
-$user_data = $response_data->data;
-
-// Cut long data into small & select only first 10 records
-$user_data = array_slice($user_data);
-
-
-
-// Print data if need to debug
-print_r($user_data);
-
-// Traverse array and display user data
-foreach ($user_data as $user) {
-	echo "name: ".$user->l2[0];
-	echo "<br />";
-	echo "name: ".$user->l3[0];
-	echo "<br /> <br />";
-}
 
 ?>
